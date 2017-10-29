2da014a4f46f36579a751ce9839eb84397b5fc85
Ryan mcmahan <owner232@gmail.com>
2017-10-29 11:02:11 -0500

    updated version # as client name

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Unitary Ecoin");
+const std::string CLIENT_NAME("Unitary Ecoin v1.0.2");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
